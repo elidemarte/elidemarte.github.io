27e42c66fd2a9ae4505210e8aa2e9f3c1c64a4c7
elidemarte <162850541+elidemarte@users.noreply.github.com>
2025-04-16 23:59:49 +0000

    add blogs

---
+++ src/content/blog/first-post.md
@@ -0,0 +1,9 @@
+---
+title: "My first blogpost"
+date: 2025-04-16
+excerpt: "This is my first blog post."
+---
+
+# Hello there 
+
+This is a blog post!
---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from 'astro:content';
+
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    excerpt: z.string(),
+    image: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  'blog': blogCollection,
+};
+
